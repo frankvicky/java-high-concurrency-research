3f74f3debc312fa418613d71c84b34286f5057a9
frankvicky <chi972121@gmail.com>
2024-04-14 23:53:44 +0800

    :heavy_plus_sign: 修改 JUnit 的編譯時間

--- netty-basic/build.gradle.kts
+++ netty-basic/build.gradle.kts
@@ -15,8 +15,8 @@ dependencies {
     implementation("ch.qos.logback:logback-core:1.5.3")
     implementation("org.slf4j:slf4j-api:2.0.12")
 
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation(platform("org.junit:junit-bom:5.10.0"))
+    implementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
